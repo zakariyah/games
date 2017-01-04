87cf1ff38a6e0c239a6ae890c2a18ad0a7b61c5c
Shoroye Zakariyah Abiodun <zakariyah@users.noreply.github.com>
2017-01-04 22:44:39 +0400

    bug fix

--- public/javascripts/ActionGUI.js
+++ public/javascripts/ActionGUI.js
@@ -513,7 +513,7 @@ var PrisonersDilemma = function()
         htmlString += "</div></div>";
         var actionsElement = document.getElementById('fullPage');
         actionsElement.innerHTML = htmlString;
-        var toCheckForNext = [['accessSkills', 'enjoy', 'familiarity', 'risk'], ['cooperative', 'forgiving', 'vengeful', 'selfish', 'predictable', 'cooperative1', 'forgiving1', 'vengeful1', 'selfish1', 'predictable1'], ['thought', 'reason1', 'preference', 'reason2'], []];
+        var toCheckForNext = [['accessSkills', 'enjoy', 'familiarity', 'risk'], ['cooperative', 'forgiving', 'vengeful', 'selfish', 'predictable', 'cooperative1', 'forgiving1', 'vengeful1', 'selfish1', 'predictable1'], ['thought', 'preference'], []];
         new Quiz('page', 4, false, false, toCheckForNext);
 	}
 
